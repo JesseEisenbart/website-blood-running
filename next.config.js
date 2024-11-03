b3564a82f6b2c592a18365d4ab669586dc4b955e
Jesse Eisenbart <jesseeisenbart@gmail.com>
2024-11-03 10:29:25 -0700

    New redirect

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ module.exports = {
 					'https://discord.gg/dead-unicorn-772603203246227547',
 				permanent: true,
 			},
+			{
+				source: '/presskit',
+				destination:
+					'https://drive.google.com/drive/folders/1BOiXRi2ysKJXfx4KrQulw0j5O4cc0vjO',
+				permanent: true,
+			},
 		];
 	},
 };
