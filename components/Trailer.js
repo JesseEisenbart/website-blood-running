02be494b51f3970930c13f2b1b33169aa081157c
Jesse Eisenbart <jesseeisenbart@gmail.com>
2025-02-07 16:16:01 -0700

    Updates

--- components/Trailer.js
+++ components/Trailer.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import Container from './Container'
+import React from 'react';
+import Container from './Container';
 
 const Trailer = () => {
-    return (
-        <Container>
-            <div className='px-0 lg:mt-24 mb-24'>
-                <div className='pb-[56.2061%] mt-12 relative'>
-                    <iframe 
-                        className='absolute top-0 left-0 w-full h-full' 
-                        src='https://www.youtube.com/embed/xiHNu4rKyDA?si=6Ku3gXYgIshklhfJ'
-                        title="YouTube video player" 
-                        style={{"border" : "0"}} 
-                        allowFullScreen
-                    />
-                </div>
-            </div>
-        </Container>
-    )
-}
+	return (
+		<Container>
+			<div className='px-0 lg:mt-24 mb-24'>
+				<div className='pb-[56.2061%] mt-12 relative'>
+					<iframe
+						className='absolute top-0 left-0 w-full h-full'
+						src='https://www.youtube.com/embed/zLQGkK0Or6A?si=n9gr_yqzOBsnMuEV'
+						title='YouTube video player'
+						style={{ border: '0' }}
+						allowFullScreen
+					/>
+				</div>
+			</div>
+		</Container>
+	);
+};
 
-export default Trailer
+export default Trailer;
